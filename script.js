7a1f42c68218c26d4d6d8c7ba0b9be0703bab69a
Dhanushkumar-23 <sdhanush4044@gmail.com>
2026-06-21 10:50:26 +0530

    Add click event to learn button for alert message

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("learnBtn");
+
+button.addEventListener("click", () => {
+    alert(
+        "She Can Foundation empowers women through education, skills, and leadership programs."
+    );
+});
